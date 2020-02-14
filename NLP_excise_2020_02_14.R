e53db6d1d4d460bd2ef2118b81441144499180b9
Cailu Lin <clin@monell.org>
2020-02-14 15:53:26 -0500

    Add files via upload

---
+++ NLP_excise_2020_02_14.R
@@ -0,0 +1,266 @@
+##https://www.datacamp.com/community/tutorials/R-nlp-machine-learning
+#data from https://github.com/eswaribala/rps_citi_ml_r2018
+#most of the libraries needed
+library(dplyr) #data manipulation
+library(ggplot2) #visualizations
+library(gridExtra) #viewing multiple plots together
+library(tidytext) #text mining
+library(wordcloud2) #creative visualizations
+
+prince_orig <- read.csv("prince_raw_data.csv", stringsAsFactors = FALSE)
+
+names(prince_orig)
+
+prince <- prince_orig %>% 
+  select(lyrics = text, song, year, album, peak, 
+         us_pop = US.Pop, us_rnb = US.R.B)
+
+glimpse(prince[139,])
+
+dim(prince)
+
+str(prince[139, ]$lyrics, nchar.max = 300)
+
+# function to expand contractions in an English-language source
+fix.contractions <- function(doc) {
+  # "won't" is a special case as it does not expand to "wo not"
+  doc <- gsub("won't", "will not", doc)
+  doc <- gsub("can't", "can not", doc)
+  doc <- gsub("n't", " not", doc)
+  doc <- gsub("'ll", " will", doc)
+  doc <- gsub("'re", " are", doc)
+  doc <- gsub("'ve", " have", doc)
+  doc <- gsub("'m", " am", doc)
+  doc <- gsub("'d", " would", doc)
+  # 's could be 'is' or could be possessive: it has no expansion
+  doc <- gsub("'s", "", doc)
+  return(doc)
+}
+
+# fix (expand) contractions
+prince$lyrics <- sapply(prince$lyrics, fix.contractions)
+
+# function to remove special characters
+removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]", " ", x)
+# remove special characters
+prince$lyrics <- sapply(prince$lyrics, removeSpecialChars)
+
+
+# convert everything to lower case
+prince$lyrics <- sapply(prince$lyrics, tolower)
+
+str(prince[139, ]$lyrics, nchar.max = 300)
+
+#get facts about the full dataset
+summary(prince)
+
+
+#create the decade column
+prince <- prince %>%
+  mutate(decade = 
+           ifelse(prince$year %in% 1978:1979, "1970s", 
+                  ifelse(prince$year %in% 1980:1989, "1980s", 
+                         ifelse(prince$year %in% 1990:1999, "1990s", 
+                                ifelse(prince$year %in% 2000:2009, "2000s", 
+                                       ifelse(prince$year %in% 2010:2015, "2010s", 
+                                              "NA"))))))
+
+#create the chart level column
+prince <- prince %>%
+  mutate(chart_level = 
+           ifelse(prince$peak %in% 1:10, "Top 10", 
+                  ifelse(prince$peak %in% 11:100, "Top 100", "Uncharted")))
+
+#create binary field called charted showing if a song hit the charts at all
+prince <- prince %>%
+  mutate(charted = 
+           ifelse(prince$peak %in% 1:100, "Charted", "Uncharted"))
+
+#save the new dataset to .csv for use in later tutorials
+write.csv(prince, file = "prince_new.csv")
+
+
+
+#define some colors to use throughout
+my_colors <- c("#E69F00", "#56B4E9", "#009E73", "#CC79A7", "#D55E00")
+
+theme_lyrics <- function() 
+{
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_blank(), 
+        axis.ticks = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "none")
+}
+
+
+
+prince %>%
+  filter(decade != "NA") %>%
+  group_by(decade, charted) %>%
+  summarise(number_of_songs = n()) %>%
+  ggplot() + 
+  geom_bar(aes(x = decade, y = number_of_songs, 
+               fill = charted), stat = "identity")  +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_blank(),
+        panel.grid.minor = element_blank()) +
+  ggtitle("Released Songs") +
+  labs(x = NULL, y = "Song Count")
+
+
+charted_songs_over_time <- prince %>%
+  filter(peak > 0) %>%
+  group_by(decade, chart_level) %>%
+  summarise(number_of_songs = n())
+
+charted_songs_over_time %>% 
+  ggplot() + 
+  geom_bar(aes(x = decade, y = number_of_songs, 
+               fill = chart_level), stat = "identity") +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_blank(),
+        panel.grid.minor = element_blank()) +
+  labs(x = NULL, y = "Song Count") +
+  ggtitle("Charted Songs")
+
+
+#look at the full data set at your disposal
+prince %>%
+  group_by(decade, chart_level) %>%
+  summarise(number_of_songs = n()) %>%
+  ggplot() +
+  geom_bar(aes(x = decade, y = number_of_songs, 
+               fill = chart_level), stat = "identity")  +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_blank(),
+        panel.grid.minor = element_blank()) +
+  labs(x = NULL, y = "Song Count") +
+  ggtitle("All Songs in Data")
+
+
+
+library(knitr) # for dynamic reporting
+library(kableExtra) # create a nicely formated HTML table
+library(formattable) # for the color_tile function
+prince %>%
+  filter(peak == "1") %>%
+  select(year, song, peak) %>%
+  arrange(year) %>%
+  mutate(year = color_tile("lightblue", "lightgreen")(year)) %>%
+  mutate(peak = color_tile("lightgreen", "lightgreen")(peak)) %>%
+  kable("html", escape = FALSE, align = "c", caption = "Prince's No. 1 Songs") %>%
+  kable_styling(bootstrap_options = 
+                  c("striped", "condensed", "bordered"), 
+                full_width = FALSE)
+
+
+undesirable_words <- c("prince", "chorus", "repeat", "lyrics", 
+                       "theres", "bridge", "fe0f", "yeah", "baby", 
+                       "alright", "wanna", "gonna", "chorus", "verse", 
+                       "whoa", "gotta", "make", "miscellaneous", "2", 
+                       "4", "ooh", "uurh", "pheromone", "poompoom", "3121", 
+                       "matic", " ai ", " ca ", " la ", "hey", " na ", 
+                       " da ", " uh ", " tin ", "  ll", "transcription",
+                       "repeats")
+
+
+head(sample(stop_words$word, 15), 15)
+
+
+#unnest and remove stop, undesirable and short words
+prince_words_filtered <- prince %>%
+  unnest_tokens(word, lyrics) %>%
+  anti_join(stop_words) %>%
+  distinct() %>%
+  filter(!word %in% undesirable_words) %>%
+  filter(nchar(word) > 3)
+
+class(prince_words_filtered)
+
+dim(prince_words_filtered)
+
+
+prince_words_filtered %>% 
+  filter(word == "race") %>%
+  select(word, song, year, peak, decade, chart_level, charted) %>%
+  arrange() %>%
+  top_n(10,song) %>%
+  mutate(song = color_tile("lightblue","lightblue")(song)) %>%
+  mutate(word = color_tile("lightgreen","lightgreen")(word)) %>%
+  kable("html", escape = FALSE, align = "c", caption = "Tokenized Format Example") %>%
+  kable_styling(bootstrap_options = 
+                  c("striped", "condensed", "bordered"), 
+                full_width = FALSE)
+
+
+
+##Word Frequency
+
+full_word_count <- prince %>%
+  unnest_tokens(word, lyrics) %>%
+  group_by(song,chart_level) %>%
+  summarise(num_words = n()) %>%
+  arrange(desc(num_words)) 
+
+full_word_count[1:10,] %>%
+  ungroup(num_words, song) %>%
+  mutate(num_words = color_bar("lightblue")(num_words)) %>%
+  mutate(song = color_tile("lightpink","lightpink")(song)) %>%
+  kable("html", escape = FALSE, align = "c", caption = "Songs With Highest Word Count") %>%
+  kable_styling(bootstrap_options = 
+                  c("striped", "condensed", "bordered"), 
+                full_width = FALSE)
+
+
+full_word_count %>%
+  ggplot() +
+  geom_histogram(aes(x = num_words, fill = chart_level )) +
+  ylab("Song Count") + 
+  xlab("Word Count per Song") +
+  ggtitle("Word Count Distribution") +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_blank(),
+        panel.grid.minor.y = element_blank())
+
+
+
+
+full_word_count %>%
+  filter(chart_level == 'Top 10' & num_words > 800) %>%
+  left_join(prince_orig, by = "song") %>%
+  select(Song = song, 
+         "Word Count" = num_words, 
+         "Peak Position" = peak, 
+         "US Pop" = US.Pop, 
+         "US R&B" = US.R.B, 
+         Canada = CA, 
+         Ireland = IR) %>%
+  kable("html", escape = FALSE) %>%
+  kable_styling(bootstrap_options = c("striped", "condensed", "bordered"))
+
+
+##Top Words
+prince_words_filtered %>%
+  count(word, sort = TRUE) %>%
+  top_n(10) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot() +
+  geom_col(aes(word, n), fill = my_colors[4]) +
+  theme(legend.position = "none", 
+        plot.title = element_text(hjust = 0.5),
+        panel.grid.major = element_blank()) +
+  xlab("") + 
+  ylab("Song Count") +
+  ggtitle("Most Frequently Used Words in Prince Lyrics") +
+  coord_flip()
+
+##Word Clouds
+
+prince_words_counts <- prince_words_filtered %>%
+  count(word, sort = TRUE) 
+
+wordcloud2(prince_words_counts[1:300, ], size = .5)
+
